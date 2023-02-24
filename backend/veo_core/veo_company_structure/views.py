7d3ce032fb46cdafd73eb77a96500b972330c86b
Stelios Bitzas <stelios@meilirobots.com>
2023-02-24 16:57:50 +0100

    basic setup and structuring of the project

--- backend/veo_core/veo_company_structure/views.py
+++ backend/veo_core/veo_company_structure/views.py
@@ -1,3 +1,10 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def index(request):
+    # dummy index view
+    html = "<html><body><h1>Index Page</h1></body></html>"
+    return HttpResponse(html)
