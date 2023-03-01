8b3fa9b34db0f898247822a1f16338dc4c953ff5
Stelios Bitzas <stelios@meilirobots.com>
2023-03-01 16:10:58 +0100

    basic functionality without calls to backend done

--- frontend/veo-company-tree/src/App.tsx
+++ frontend/veo-company-tree/src/App.tsx
@@ -1,11 +1,58 @@
 import Tree from "./components/tree";
-// eslint-disable-next-line import/no-unresolved
+import { useState } from "react";
+
+
 import tree_json from "./Data/tree_outputs";
+import Form from "./components/form";
+
+export type selectedProps = {
+  id: number;
+  name: string;
+};
 
 export default function App() {
+  const [selected , setSelected]  = useState<selectedProps>({id:-1, name:"None"});
+
+  
+  
   return (
-    <div className="App">
-      <Tree data={tree_json.trees[0]} />
-    </div>
+    <>
+      <nav
+        className="navbar navbar-light"
+        style={{ backgroundColor: "#e3f2fd", marginBottom:"20px"}}
+      >
+        <div className="container">
+          <a className="navbar-brand" href="/">
+            <img src={process.env.PUBLIC_URL + "/logo.png"} alt="" width="50" />{" "}
+            Veo Assignment
+          </a>
+        </div>
+      </nav>
+      <div
+        className="App container"
+      >
+        <div className="row">
+          <div className="col-6">
+            <h1>Company Structure</h1>
+            <Tree
+              data={tree_json.trees[0]}
+              selectedState={{ selected, setSelected }}
+            />
+          </div>
+          <div className="wrapper col-6">
+            <h2>Create a child node</h2>
+            <div style={{ color: "black" }}>
+              <h5 style={{ color: "gray" }}>
+                Selected Parent Node:{" "}
+                <span style={{ fontWeight: "600", color: "lightskyblue" }}>
+                  {selected.id !== -1 ? selected.name : "None"}
+                </span>
+              </h5>
+            </div>
+            <Form selected={selected} />
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
--- frontend/veo-company-tree/src/components/tree.tsx
+++ frontend/veo-company-tree/src/components/tree.tsx
@@ -1,23 +1,54 @@
-import { useState } from "react";
+import { useEffect, useRef } from "react";
 
 type dataProps = {
     name: string;
+    id: number;
     sub_tree: dataProps[];
 }
 
-function Tree({ data }: { data: dataProps}) {
-  const [expand, setExpand] = useState(false);
+export default function Tree({
+  data,
+  selectedState,
+}: {
+  data: dataProps;
+  selectedState: { selected: { id: number; name: string }; setSelected: (arg0: { id: number; name: string }) => void };
+}) {
+  const { selected, setSelected } = selectedState;
+
+  const handleClick = () => {
+    setSelected({ id: data.id, name: data.name });
+  };
+
+  useEffect(() => {
+    if (selected.id === data.id) {
+      console.log("selected");
+      console.log(selected.id, selected.name);
+    }
+  }, [selected, data.id, , setSelected]);
+
+
+  
   return (
     <div>
-      <span>{data.name}</span>
-      <br />
-      <div style={{ display:  "block", paddingLeft: 15 }}>
+      <button
+        className="btn button-light"
+        style={{
+          margin: "2px",
+          border: "1px solid grey",
+          borderRadius: "5px",
+          backgroundColor:
+            selected.id === data.id ? "LightSkyBlue" : "transparent",
+        }}
+        onClick={handleClick}
+      >
+        {data.name}
+      </button >
+      <div style={{ display: "block", paddingLeft: 20 }}>
         {data.sub_tree.map((tree) => (
-          <Tree key={tree.name} data={tree} />
+          <Tree key={tree.name} data={tree} selectedState={selectedState} />
         ))}
       </div>
     </div>
   );
 }
 
-export default Tree;
